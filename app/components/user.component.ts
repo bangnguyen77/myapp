e358e3760fe2ea66944819deb87812bb0730b052
Bang Nguyen <bangnv77@gmail.com>
2016-12-20 23:13:34 -0500

    use ngFor to loop through all hobbies

--- app/components/user.component.ts
+++ app/components/user.component.ts
@@ -6,12 +6,19 @@ import { Component } from '@angular/core';
   <h1>Hello {{name}}</h1>
   <p><strong>Email:</strong> {{email}}</p>
   <p><strong>Address:</strong> {{address.street}} {{address.city}}, {{address.state}}</p>
+  <h3>Hobbies</h3>
+  <ul>
+    <li *ngFor="let hobby of hobbies">
+      {{hobby}}
+    </li>
+  </ul>
   `,
 })
 export class UserComponent  {
   name: string;
   email: string;
-  address: address;
+  address: address; // address property is of type address
+  hobbies: string[];
 
   constructor() {
     this.name = 'John Doe';
@@ -20,7 +27,8 @@ export class UserComponent  {
       street: '12 Main St',
       city: 'Boston',
       state: 'MA'
-    }
+    };
+    this.hobbies = ['Music', 'Movies', 'Sports'];
   }
 }
 
