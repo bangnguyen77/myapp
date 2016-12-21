4b468ce3eabaa8fc778b39e01d7e2306e61db78e
Bang Nguyen <bangnv77@gmail.com>
2016-12-20 22:48:49 -0500

    create UserComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,18 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>Hello {{name}}</h1>
-  <p><strong>Email:</strong> {{email}}</p>
-  <p><strong>Address:</strong> {{address.street}} {{address.city}}, {{address.state}}</p>
+    <user></user>
   `,
 })
 export class AppComponent  {
-  name = 'John Doe';
-  email = 'john@gmail.com';
-  address = {
-    street: '12 Main St',
-    city: 'Boston',
-    state: 'MA'
-  }
 
 }
