ffecf5ea76dce9a4f03a0d341672d8e48a50cf90
Bang Nguyen <bangnv77@gmail.com>
2016-12-21 00:01:38 -0500

    create delete-hobby functionality

--- app/components/user.component.ts
+++ app/components/user.component.ts
@@ -11,8 +11,8 @@ import { Component } from '@angular/core';
   <div *ngIf="showHobbies">
     <h3>Hobbies</h3>
     <ul>
-      <li *ngFor="let hobby of hobbies">
-        {{hobby}}
+      <li *ngFor="let hobby of hobbies; let i=index">
+        {{hobby}} <button (click)="deleteHobby(i)">X</button>
       </li>
     </ul>
     <form (submit)="addHobby(hobby.value)">
@@ -66,6 +66,10 @@ export class UserComponent  {
 
   addHobby(hobby) {
     this.hobbies.push(hobby);
+  }
+
+  deleteHobby(i) {
+    this.hobbies.splice(i, 1);
   }
 }
 
