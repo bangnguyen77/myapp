bb756fa1b5412d1b4cd420b6af8ef7a90566dcda
Bang Nguyen <bangnv77@gmail.com>
2016-12-20 22:42:21 -0500

    add email and address properties to AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>Hello {{name}}</h1>
+  <p><strong>Email:</strong> {{email}}</p>
+  <p><strong>Address:</strong> {{address.street}} {{address.city}}, {{address.state}}</p>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'John Doe';
+  email = 'john@gmail.com';
+  address = {
+    street: '12 Main St',
+    city: 'Boston',
+    state: 'MA'
+  }
+
+}
